64c46f1de49a1e50f56d495061cbee11025dce47
easeghehey <emanuelaseghey9@gmail.com>
2022-10-10 21:33:26 -0400

    tree

--- Graph/Dijkstra.py
+++ Graph/Dijkstra.py
@@ -1,6 +1,6 @@
 import heapq
 from collections import defaultdict
-arr = [[1,2,10],[2,0,7],[1,3,8],[4,0,10],[3,4,2],[4,2,10],[0,3,3],[3,1,6],[2,4,5]]
+arr = [[3,5,78],[2,1,1],[1,3,0],[4,3,59],[5,3,85],[5,2,22],[2,4,23],[1,4,43],[4,5,75],[5,1,15],[1,5,91],[4,1,16],[3,2,98],[3,4,22],[5,4,31],[1,2,0],[2,5,4],[4,2,51],[3,1,36],[2,3,59]]
 
 def clean(arr):
     adjlist = defaultdict(list)
@@ -9,7 +9,7 @@ def clean(arr):
     return adjlist
 
 def dijkstra(graph, src, dst, n):
-    dist = {i: float('inf') for i in range(n)}
+    dist = {i: float('inf') for i in range(1, n + 1)}
     visited = set()
 
     pqueue = [[src, 0]]
@@ -17,12 +17,17 @@ def dijkstra(graph, src, dst, n):
 
     while pqueue:
         cur, cst = heapq.heappop(pqueue)
+        
+        if cur in visited:
+            continue
+
         visited.add(cur)
         dist[cur] = min(dist[cur], cst)
+
         for neighbor, cost in graph[cur]:
             if neighbor not in visited:
                 heapq.heappush(pqueue, [neighbor, cost + cst])
     print(dist)
     return dist[dst]
 
-print(dijkstra(clean(arr), 1, 4, 5))
+print(dijkstra(clean(arr), 5, 4, 5))
